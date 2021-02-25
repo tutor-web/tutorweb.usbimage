f0e8bef315aae54b7bb1b829b120cfc7272920ce
Jamie Lentin <jm@lentin.co.uk>
2021-02-25 12:18:49 +0000

    smileycoin: Roughly-working smileycoin
    Prequisites are there I think, but not compiling due to bugs in
    smileycoin itself.

--- conf/preboot-smileycoin.sh
+++ conf/preboot-smileycoin.sh
@@ -2,17 +2,31 @@
 # Run in ~chroot env on image creation
 set -eu
 
+apt-get install -y git \
+    build-essential pkg-config \
+    autoconf automake autotools-dev libtool \
+    libdb++-dev libboost-all-dev \
+    libssl-dev
+adduser --system smly
+
+git clone git://github.com/smileycoin/smileyCoin /srv/smileycoin
+(cd /srv/smileycoin && ./autogen.sh && ./configure && make && ./install.sh)
+
 cat <<'EOSH' >> /usr/local/sbin/smileycoin-setup
 #!/bin/sh
 
-[ -d /var/lib/smly ] || {
-  mkdir -p /var/local/var/lib/smly
-  chown smly:staff /var/local/var/lib/smly
-}
-[ -f /var/lib/smly/smileycoin.conf ] || {
-    RPCPASS="$(xxd -ps -l 22 /dev/urandom)"
-    echo "rpcuser=smileycoinrpc" > /var/lib/smly/smileycoin.conf
-    echo "rpcpassword=${RPCPASS}" >> /var/lib/smly/smileycoin.conf
-}
+exit 0
 EOSH
 chmod +x /usr/local/sbin/smileycoin-setup
+
+mkdir -p /etc/systemd/system/smly.service.d; cat <<'EOF' > /etc/systemd/system/smly.service.d/override.conf
+[Service]
+ExecStartPre=/usr/local/sbin/smileycoin-setup
+EOF
+
+# TODO:
+# sudo -u smly /srv/tutorweb.smileycoin/src/smileycoind \
+#     -datadir=/var/lib/smly getnewaddress ""
+# sudo -u smly /srv/tutorweb.smileycoin/src/smileycoind \
+#     -datadir=/var/lib/smly encryptwallet (key)
+# TODO: Write wallet pwd to conf
