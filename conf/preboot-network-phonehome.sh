38edfaa35eadfbcecf98eda4e72ad2a410574310
Jamie Lentin <jm@lentin.co.uk>
2021-04-28 10:05:08 +0100

    network-phonehome: Socket configuration is server-side
    Add some rough documentation on what happens there.

--- conf/preboot-network-phonehome.sh
+++ conf/preboot-network-phonehome.sh
@@ -2,6 +2,23 @@
 # Run in ~chroot env on image creation
 set -eu
 
+# Remote configuration:
+# adduser --disabled-password --home /home/phonehome --shell /bin/true --ingroup nogroup phonehome
+# Add key to /home/phonehome/.ssh/authorized_keys
+# cat <<EOF >> /etc/ssh/sshd_config
+# Match User phonehome
+#     ChrootDirectory /home/phonehome
+#     StreamLocalBindUnlink yes
+#     StreamLocalBindMask 0111
+# EOF
+# cat <<EOF > /home/phonehome/true.c
+# /* gcc true.c -static -o ./bin/true */
+# int main () { return 0; }
+# gcc /home/phonehome/true.c -static -o /home/phonehome/bin/true
+
+# Connecting from remote:
+# ssh -o "ProxyCommand socat - UNIX-CLIENT:/home/phonehome/tmp/(host)-ssh" tutor@localhost
+
 # Populate known_hosts, checking we can connect in the process
 chmod a+r /twpreload/phonehome/id_rsa
 mkdir -p /etc/phonehome
@@ -26,11 +43,11 @@ Type=simple
 ExecStart=/bin/sh -c 'exec /usr/bin/ssh -NTC \
     -o ServerAliveInterval=60 \
     -o ExitOnForwardFailure=yes \
-    -o StreamLocalBindUnlink=yes \
     -o UserKnownHostsFile=/etc/phonehome/known_hosts \
     -i /twpreload/phonehome/id_rsa \
     -L 9025:localhost:25 \
-    -R /tmp/phonehome-$$HOSTNAME-%m:localhost:22 \
+    -R /tmp/$$(hostname)-ssh:localhost:22 \
+    -R /tmp/$$(hostname)-http:localhost:80 \
     -F /twpreload/phonehome/config \
     phonehome'
 
